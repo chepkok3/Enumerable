aef550900c2d3d11d73cbb8e39cf44dbcbab3e47
amklass11 <lassanaa14@gmail.com>
2023-04-14 08:17:56 +0100

    enumerables

--- my_list.rb
+++ my_list.rb
@@ -1,15 +1,33 @@
-require_relative 'my_enumerable'
+require_relative 'enumerable'
 
 class MyList
   include MyEnumerable
 
-  def initialize(*args)
-    @list = args
+  def initialize(*list)
+    @list = list
   end
 
-  def each(&block)
-    return to_enum(:each) unless block_given?
-
-    @list.each(&block)
+  def each
+    (0...@list.length).each do |i|
+      yield @list[i]
+    end
   end
 end
+
+# Verify solution:
+# Create our list
+list = MyList.new(1, 2, 3, 4)
+# <MyList: @list=[1, 2, 3, 4]>
+# Test #all?
+list.my_all? { |e| e < 5 }
+# true
+list.my_all? { |e| e > 5 }
+# false
+# Test #any?
+list.my_any? { |e| e == 2 }
+# true
+list.my_any? { |e| e == 5 }
+# false
+# Test #filter
+list.my_filter?(&:even?)
+# [2, 4]
